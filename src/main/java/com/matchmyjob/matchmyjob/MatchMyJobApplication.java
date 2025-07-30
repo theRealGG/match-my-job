9d2ad47d2ee63d9806e603a9194b62064e94a5c9
Hisham Görner <hisham30g@gmail.com>
2025-07-30 18:35:50 +0200

    feat: initial commit

---
+++ src/main/java/com/matchmyjob/matchmyjob/MatchMyJobApplication.java
@@ -0,0 +1,13 @@
+package com.matchmyjob.matchmyjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchMyJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchMyJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matchmyjob/matchmyjob/MatchMyJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchmyjob.matchmyjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchMyJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
